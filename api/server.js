3f91c7f63bc2d01a141d36cfaa3f999e0893345b
SherazKarim <sherazkarimz@gmail.com>
2024-01-08 23:56:56 +0500

    fix: update cors origin

--- api/server.js
+++ api/server.js
@@ -19,7 +19,7 @@ const connect = async()=>{
 }
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:"http://localhost:3000",credentials:true}))
+app.use(cors({origin:"*",credentials:true}))
 
 app.use('/api/auth',authRoute)
 app.use('/api/user',newUser)
