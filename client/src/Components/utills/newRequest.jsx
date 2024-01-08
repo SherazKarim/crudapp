779ae00d1cf5d7a16d2cecbd1a77329cf1bf343c
SherazKarim <sherazkarimz@gmail.com>
2024-01-09 00:35:01 +0500

    fix: baseUrl withCredentials

--- client/src/Components/utills/newRequest.jsx
+++ client/src/Components/utills/newRequest.jsx
@@ -3,5 +3,5 @@ import axios from 'axios'
 export const newRequest = axios.create({
     // baseURL:"http://localhost:8800/api/",
     baseURL:"https://crudapp-production-db62.up.railway.app/api/",
-    withCredentials:true
+    // withCredentials:true
 })
